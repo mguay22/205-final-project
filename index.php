d21186d79b6e718a1be2e288647415a0d89e90ea
joshuachilds <jhchilds@uvm.edu>
2019-03-27 12:50:32 -0400

    start to integrate database structure into auth structure

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-require_once(__DIR__ . '/templates/header.php');
-require_once(__DIR__ . '/templates/footer.php');
+require_once(__DIR__ . '/templates/top.php');
 ?>
 
 <a href="templates/login.php">Sign In</a>
+
+<?php
+require_once(__DIR__ . '/templates/footer.php');
+?>
