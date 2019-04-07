b036f8348b404b42cc1785fafadc8efbfc20cce9
mguay22 <guay.me@gmail.com>
2019-04-07 12:12:08 -0400

    Update

--- lib/constants.php
+++ lib/constants.php
@@ -3,7 +3,7 @@ define('DATABASE_NAME', 'MGUAY_BillApp');
 define('DATABASE_READER', 'mguay_reader');
 define('DATABASE_READER_PWD', 'r');
 define('DATABASE_WRITER_PWD', 'w');
-define('DATABASE_WRITER', 'jhchilds_writer');
+define('DATABASE_WRITER', 'mguay_reader');
 
 define('ADMIN_EMAIL', get_current_user() . '@uvm.edu');
 
